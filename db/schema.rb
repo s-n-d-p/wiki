a50f8c61ad97f4af250603356aaffd19b027ff88
Sandeep <sandeep_b150487cs@nitc.ac.in>
2019-03-29 11:07:29 +0530

    added edit article and made partial for form (new/edit)

--- db/schema.rb
+++ db/schema.rb
@@ -16,17 +16,9 @@ ActiveRecord::Schema.define(version: 2019_03_29_023557) do
     t.string "title"
     t.text "body"
     t.integer "user_id"
-    t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_articles_on_category_id"
     t.index ["user_id"], name: "index_articles_on_user_id"
-  end
-
-  create_table "categories", force: :cascade do |t|
-    t.string "category"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
