e4d9515b8fc8c87e687eb7ee0f0cfa9a29ab6a62
Sandeep <sandeep_b150487cs@nitc.ac.in>
2019-04-03 11:59:56 +0530

    tweaked ui with bootstrap, tags now link to tag-filter page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-    before_action :authenticate_user!, except: [:index, :show]
+    before_action :authenticate_user!, except: [:index, :show, :show_category]
 
     def index 
         @articles = Article.all.order('created_at DESC')
